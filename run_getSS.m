e790a56baae2ce396289ba1b15d563d3c2d1848b
sdevinyak <96308955+sdevinyak@users.noreply.github.com>
2022-03-24 15:55:02 -0400

    Update run_getSS.m

--- run_getSS.m
+++ run_getSS.m
@@ -3,17 +3,6 @@
 %% Begin user input
 pars = set_params();
 
-% trying fitted params
-pars.Phi_dtKsec_eq= 0.041;%0.043;%0.0427;%0.043;
-pars.Phi_cdKsec_eq= 0.0022;%0.0018;%0.002;%0.002;
-
-pars.FF = 0.250274;   
-pars.insulin_A = 1;%0.999045;
-pars.insulin_B = 0.6753 ; %0.6645 makes rho ins 1;%
-pars.cdKreab_A = 0.295650;
-pars.cdKreab_B = 0.472825;
-pars.Kec_total = 4.2;
-
 
 Kin.Kin_type = 'gut_Kin3';
 Kin.Meal = 1;
